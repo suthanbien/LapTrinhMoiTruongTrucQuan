315ab5b698151fc0febb713bc36f314b1f2349ce
Quy <suthanbien@gmail.com>
2020-03-12 15:04:22 +0700

    Linh Tinh

--- Tuan2/Lab2_Interface/Lab2_Interface/Listener.cs
+++ Tuan2/Lab2_Interface/Lab2_Interface/Listener.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lap2_Delegate
+namespace Lab2_Interface
 {
     class Listener
     {
--- Tuan2/Lab2_Interface/Lab2_Interface/Program.cs
+++ Tuan2/Lab2_Interface/Lab2_Interface/Program.cs
@@ -8,25 +8,44 @@ namespace Lab2_Interface
 {
     class Program
     {
+        private delegate void NotifyMe(string s1);
         static void Main(string[] args)
         {
-            IHuman h;
-            Theodore t;
+            /* IHuman h;
+             Theodore t;
 
-            h = new Theodore();
+             h = new Theodore();
 
-            //cast for assignement
-            t = (Theodore)h;
+             //cast for assignement
+             t = (Theodore)h;
 
-            //use class based reference
-            t.Name = "Fred";
-            t.Speak("I like public implementations!");
+             //use class based reference
+             t.Name = "Fred";
+             t.Speak("I like public implementations!");
 
-            //use interface reference
-            h.Name = "Teddy";
-            h.Speak("I like VB!");
+             //use interface reference
+             h.Name = "Teddy";
+             h.Speak("I like VB!");*/
+
+
+            NotifyMe d = new NotifyMe(Listener.GetNotifiedAgain);
+           
+         
+            InvokeDelegate(d);
+           
+            Listener lsnr = new Listener();
+          
+            NotifyMe d2 = new NotifyMe(lsnr.GetNotified);
+         
+            InvokeDelegate(d2);
+
+            InvokeDelegate(d);
 
             Console.Read();
+        }
+        static void InvokeDelegate(NotifyMe d)
+        {
+            d("You are late paying your bills!");
         }
     }
 }
